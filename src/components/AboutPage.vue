44513a8ca5b18c116bbc274b2c72db2a83647ed5
Khunaiko <pasuaiko21089@gmail.com>
2023-10-24 15:46:42 +0700

    first commit

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1>About Page</h1>
+</div>
+</template>
+
+<script>
+export default {
+        name: "AboutPage",
+    };
+</script>
+
+<style>
+    h1{
+        color: blueviolet;
+    }
+</style>
---
+++ src/components/ContactPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1> Contact Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+        name: "ContactPage",
+    };
+</script>
+
+<style>
+    h1{
+        color: blueviolet;
+    }
+</style>
